550baf280f8999629e1495f26706b5db9e7681a8
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-22 19:12:41 -0300

    ajuste na função shortenUrl

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -9,14 +9,14 @@ export async function shortenUrl(req, res) {
     try {
         const token = res.locals.session.token
 
-        const dataUserSession = await db.query(`SELECT * FROM sessions WHERE token=$1;`, [token])
+        const userId = await db.query(`SELECT "userId" FROM sessions WHERE token=$1;`, [token])
 
-        await db.query(`INSERT INTO "shortedUrls" ("userId", url, "shortUrl")
-        VALUES ($1, $2, $3);`, [dataUserSession.rows[0].userId, url, shortUrl])
+        await db.query(`INSERT INTO shortedUrls ("userId", url, "shortUrl")
+        VALUES ($1, $2, $3);`, [userId, url, shortUrl])
 
-        const dataShortUrl = await db.query(`SELECT * FROM "shortedUrls" WHERE "shortUrl" = $1;`, [shortUrl])
+        const idUrl = await db.query(`SELECT id FROM shortedUrls WHERE "shortUrl" = $1;`, [shortUrl])
 
-        res.status(201).send({ id: dataShortUrl.rows[0].id, shortUrl })
+        res.status(201).send({ id: idUrl, shortUrl })
     } catch (err) {
         res.sendStatus(500)
     }
@@ -27,7 +27,7 @@ export async function getUrlById(req, res) {
     const { id } = req.params
 
     try {
-        const infoUrls = await db.query(`SELECT id, "shortUrl", url FROM "shortedUrls" WHERE id=$1;`, [id])
+        const infoUrls = await db.query(`SELECT id, shortUrl, url FROM "shortedUrls" WHERE id=$1;`, [id])
 
         if (infoUrls.rows.length === 0) return res.status(404).send("URL não existe")
         res.status(201).send(infoUrls.rows[0])
@@ -41,10 +41,10 @@ export async function openUrl(req, res) {
     const { shortUrl } = req.body;
 
     try {
-        const infoUrl = await db.query(`SELECT * FROM "shortedUrls" WHERE "shortUrl" = $1;`,[shortUrl])
+        const infoUrl = await db.query(`SELECT * FROM shortedUrls WHERE "shortUrl" = $1;`,[shortUrl])
         if (infoUrl.rows.length === 0) return res.sendStatus(404);
 
-        await db.query(`UPDATE "shortedUrls" SET "visitCount" = Number("visitCount")+1 WHERE "shortUrl"=$1;`,[shortUrl])
+        await db.query(`UPDATE shortedUrls SET "visitCount" = Number("visitCount")+1 WHERE "shortUrl"=$1;`,[shortUrl])
         const url = infoUrl.rows[0].url
         return res.redirect(url);
     } catch (err) {
@@ -58,7 +58,7 @@ export async function deleteUrl(req, res) {
 
     try {
 
-        const url = await db.query(`SELECT * FROM "shortedUrls" WHERE id = $1;`,[id])
+        const url = await db.query(`SELECT * FROM shortedUrls WHERE id = $1;`,[id])
         
         if (url.rows.length === 0) return res.sendStatus(404)
         
