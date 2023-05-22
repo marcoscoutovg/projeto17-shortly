5c5dda59fe928b379306dc0037936c15013e09ce
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-22 00:10:02 -0300

    dump do banco de dados

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -2,7 +2,9 @@ import { db } from "../database/database.connection";
 
 export async function getRanking(req, res) {
     try {
+        await db.query(`SELECT id AND name`)
 
+        res.status(200).send(ranking)
     } catch(err) {
         res.sendStatus(500)
     }
