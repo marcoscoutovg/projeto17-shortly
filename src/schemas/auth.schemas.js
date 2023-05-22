647c753f62d870ab2e50a5e98211986496c65b4f
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-22 13:30:09 -0300

    novos ajustes

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -4,7 +4,7 @@ export const registerSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().min(3).required(),
-  confirmPassword: joi.string().valid.(joi.ref("password")).required()
+  confirmPassword: joi.string().valid(joi.ref("password")).required()
 });
 
 export const loginSchema = joi.object({
