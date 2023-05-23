0c71308990afe723aea8ff59ab661f1d09863de8
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-23 14:34:38 -0300

    versão final

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authValidate } from "../middlewares/auth.middlewares.js";
+import { me } from "../controllers/users.controllers.js";
 
 const usersRouter = Router()
 
-usersRouter.get("/users/me", authValidate)
+usersRouter.get("/users/me", authValidate, me)
 
 export default usersRouter;
